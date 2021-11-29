e376fe684b45d0f640e8cb8ec65f14d6fc5707c2
sagarjethideveloper <recsagarjethi@gmail.com>
2021-11-29 18:12:08 +0530

    first commit

---
+++ js/globals.js
@@ -0,0 +1,10 @@
+const RPC_ENDPOINT = "https://anudit-chainwizard-6hp8-9933.githubpreview.dev"
+const WS_ENDPOINT = "wss://anudit-chainwizard-6hp8-9944.githubpreview.dev"
+// const RPC_ENDPOINT = "https://kovan.infura.io/v3/"
+// const WS_ENDPOINT = "wss://kovan.infura.io/ws/v3/"
+const contractABI="";
+const contractAddress = "";
+
+const COORDINATOR_NODE = "http://127.0.0.1:5000/";
+const upload_ep = "first-run";
+const train_ep = "sendtrain";
